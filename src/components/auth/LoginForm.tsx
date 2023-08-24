d7cde34997fb399932f729e1b064ee7c7746bdac
alexhetley6107 <hetleyofficial@gmail.com>
2023-08-24 09:38:50 +0300

    feat(loginForm)

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { FC } from 'react';
+import s from './Auth.module.scss';
+import { Form, Input, Button } from 'antd';
 
-type Props = {};
+export const LoginForm: FC = () => {
+  const onSubmit = async (values: any) => {
+    console.log(values);
+  };
 
-export const LoginForm = (props: Props) => {
-  return <div>Login</div>;
+  return (
+    <div className={s.formBlock}>
+      <Form name="basic" labelCol={{ span: 8 }} onFinish={onSubmit}>
+        <Form.Item
+          label="E-Mail"
+          name="email"
+          rules={[{ required: true, message: 'Email is required' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Password is required',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
