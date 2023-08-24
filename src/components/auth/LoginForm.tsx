5ba67c74915cf5961303fc0b0d541eb709064b5e
alexhetley6107 <hetleyofficial@gmail.com>
2023-08-24 09:57:38 +0300

    feat(login)

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from 'react';
+import { Form, Input, Button, notification } from 'antd';
+import { setCookie } from 'nookies';
+
 import s from './Auth.module.scss';
-import { Form, Input, Button } from 'antd';
+import { LoginFormDTO } from '@/api/dto/auth.dto';
+import * as Api from '@/api';
 
 export const LoginForm: FC = () => {
-  const onSubmit = async (values: any) => {
-    console.log(values);
+  const onSubmit = async (values: LoginFormDTO) => {
+    try {
+      const { token } = await Api.auth.login(values);
+
+      notification.success({
+        message: 'Success!',
+        description: 'Going to admin-panel...',
+        duration: 2,
+      });
+
+      setCookie(null, '_token', token, {
+        path: '/',
+      });
+    } catch (error) {
+      console.warn('LoginForm', error);
+
+      notification.error({
+        message: 'Error!',
+        description: 'Wrong email or password',
+        duration: 2,
+      });
+    }
   };
 
   return (
